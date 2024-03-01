0c9d229479763ed6504a3c1e3737ba6cb18216fb
unknown <abinashbhattarai3@gmail.com>
2024-03-01 19:47:39 +0545

    Todo App

---
+++ script.js
@@ -0,0 +1,79 @@
+const userInput = document.querySelector(".todo-input input");
+const userOutput = document.querySelector(".todo-task");
+const editPend = document.querySelector(".editPend");
+const clearAll = document.querySelector(".clearBtn");
+
+let todos = JSON.parse(localStorage.getItem("todos")) || [];
+let editMode = false;
+let editIndex;
+
+userInput.addEventListener("keyup", e => {
+  let inputTask = userInput.value.trim();
+  if(e.key == "Enter" && inputTask){
+    if(editMode) {
+      todos[editIndex].name = inputTask;
+      editMode = false;
+      editPend.style.display = "none";
+    }
+    else{
+      todos.push({name: inputTask, status: "pending"});
+    }
+    localStorage.setItem("todos", JSON.stringify(todos));
+    userInput.value = "";
+  }
+  getTasks();
+})
+
+const getTasks = () => {
+  let outputTask = "";
+  
+  todos.forEach((task, index) => {
+    outputTask += `<li>
+                    <label for="${index}">
+                      <input type="checkbox" id="${index}" onclick="setComplete(this)">
+                      <p>${task.name}</p>
+                    </label>
+                    <div>
+                    <span class="material-symbols-outlined" onclick="editTask(${index})">edit</span>
+                    <span class="material-symbols-outlined" onclick="deleteTask(${index})">delete</span>
+                    </div>
+                  </li>`
+  })
+  userOutput.innerHTML = outputTask || `<span>You don't have any task...</span>`;
+}
+
+const setComplete = (selectedTask) => {
+  let taskName = selectedTask.parentElement.lastElementChild;
+    if(selectedTask.checked){
+      taskName.classList.add("line-through");
+      todos[selectedTask.id].status = "completed";
+    }
+    else{
+      taskName.classList.remove("line-through");
+      todos[selectedTask.id].status = "pending";
+    }
+    localStorage.setItem("todos", JSON.stringify(todos));
+};
+
+const deleteTask = (index) => {
+  todos.splice(index, 1);
+  localStorage.setItem("todos", JSON.stringify(todos));
+  getTasks(todos);
+};
+
+const editTask = (index) => {
+  editPend.style.display = "block";
+  editMode = true;
+  editIndex = index;
+  userInput.value = todos[index].name;
+}
+
+clearAll.addEventListener("click", () => {
+  while(todos.length>0){
+    todos.pop();
+  }
+  localStorage.setItem("todos", JSON.stringify(todos));
+  getTasks();
+})
+
+getTasks();
